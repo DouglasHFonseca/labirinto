830809d9c0a6cf3669c11f593a08fe6fbd57d063
Douglas <douglas.hfonseca@gmail.com>
2021-08-16 21:37:45 -0300

    fix: bug path

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 #include "mouseMovement.h"
 #include "userStack.h"
 #include "interfaceGrafica.h"
-int main() {
-
-
+int main()
+{
 
     TUserStack userStack;
     FPVazia2(&userStack);
 
-    TMazeStack  mazeStack;
+    TMazeStack mazeStack;
     FPVazia(&mazeStack);
 
     TMazeCell currentCell, exitCell;
@@ -20,10 +19,12 @@ int main() {
     printf("Exemplo, 0000 tera 4 colunas.\n");
     printf("Recomenda-se rodar no terminal.\n\n");
 
-    do{
+    do
+    {
         printf("\nDigite a 1 linha: ");
         leLinha(&userStack);
-        if(!(isPilhaVazia2(userStack))){
+        if (!(isPilhaVazia2(userStack)))
+        {
             break;
         }
     } while (1);
@@ -31,73 +32,75 @@ int main() {
     int rows, columns;
     columns = userStack.profundidade;
     rows = columns;
-    char** maze = malloc((sizeof (char*)) * (rows+2));
+    char **maze = malloc((sizeof(char *)) * (rows + 2));
 
+    for (int i = 0; i < rows + 2; i++)
+        maze[i] = (char *)malloc((columns + 2) * sizeof(char));
 
-    for (int i=0; i<rows+2; i++)
-        maze[i] = (char *)malloc((columns+2) * sizeof(char));
-
-    for(int i = 0; i < rows+2; i++){
-        for(int j = 0; j < columns+2; j++){
+    for (int i = 0; i < rows + 2; i++)
+    {
+        for (int j = 0; j < columns + 2; j++)
+        {
             maze[i][j] = '1';
         }
     }
 
-    int m = leLinhas(&userStack, maze, rows, &currentCell, &exitCell) ;
-    if(m == -1){
+    int m = leLinhas(&userStack, maze, rows, &currentCell, &exitCell);
+    if (m == -1)
+    {
         printf("\nProgram will terminate.");
         printf("\nPress enter to continue...");
-        for (int i = 0; i <  rows; i++)
+        for (int i = 0; i < rows; i++)
             free(maze[i]);
 
         free(maze);
         getchar();
         return 0;
     }
-    int height = rows+1;
-    int lenght = columns+1;
+    int height = rows + 1;
+    int lenght = columns + 1;
 
     TCoordenadas limitCoord;
 
-    limitCoord.y = rows+3;
-    limitCoord.x = columns+3;
-
+    limitCoord.y = rows + 3;
+    limitCoord.x = columns + 3;
 
     int isTherePath;
-    do{
+    do
+    {
         isTherePath = moveMouse(maze, &currentCell, exitCell, &mazeStack, limitCoord);
         printf("\n");
         system("cls");
-        showDisplay(height,lenght, maze);
+        showDisplay(height, lenght, maze);
         sleep(1);
     } while (isTherePath == 0);
 
-
-    if(isTherePath == 1){
-        mgotoxy(0, height+2);
+    if (isTherePath == 1)
+    {
+        mgotoxy(0, height + 2);
         printf("\nPath found!! The mouse has escaped!!\n");
         printf("Press enter to continue");
         getchar();
     }
-    if(isTherePath == -1){
-        mgotoxy(0, height+2);
+    if (isTherePath == -1)
+    {
+        mgotoxy(0, height + 2);
         printf("\n\nThere is no path... Beyond the scope of light, beneath the reach of dark."
                "\nWhat could possibly await us, yet we seek it, insatiably... For that's our curse.\n\n");
         printf("Press enter to continue");
         getchar();
     }
 
-
-    for (int i = 0; i < columns+2; i++) {
-        for(int j = 0; j < rows+2; j++){
+    for (int i = 0; i < columns + 2; i++)
+    {
+        for (int j = 0; j < rows + 2; j++)
+        {
             printf("%c", maze[i][j]);
         }
         printf("\n");
     }
 
-
-
-    for (int i = 0; i <  rows; i++)
+    for (int i = 0; i < rows; i++)
         free(maze[i]);
 
     free(maze);
